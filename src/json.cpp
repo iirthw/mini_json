2c965aea3714b479db50beb05b20a3ae400bf582
iirthw <iirthw@gmail.com>
2021-03-22 21:01:08 +0200

    Add src dir structure

---
+++ src/json.cpp
@@ -0,0 +1,6 @@
+#include "json.h"
+
+namespace mini
+{
+
+} // namespace mini
---
+++ src/json.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <srting>
+
+namespace mini
+{
+	class Json
+	{
+		public:
+			explicit Json(const std::string& str);
+
+			Json parse(const std::string& attrib) const;
+			bool value() const;
+			int value() const;
+			float value() const;
+			std::string value() const;
+			size_t numObjects() const;
+			Json operator[] (int index) const;
+
+		private:
+			static std::string readTextFile(const std::string& filePath);
+			static Json fromFile(const std::string& filePath);
+	}; // class Json
+} // namespace mini
