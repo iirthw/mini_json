cf06ecf447fa8fa4eaa8d6aaa2fc5692567c0af6
iirthw <iirthw@gmail.com>
2021-03-23 21:47:03 +0200

    Add quick (but broken) version of ExternalProject_Add for GoogleTest

--- cmake/find_gtest.cmake
+++ cmake/find_gtest.cmake
@@ -49,4 +49,21 @@ if ( DEFINED GTEST_FORCE_EXTERNAL )
     	# Disable install step
     	INSTALL_COMMAND ""
     )
-endif()
+
+    # Get GTest source and binary directories from CMake project
+    ExternalProject_Get_Property(gtest source_dir binary_dir)
+
+    # Create a libgtest target to be used as a dependency by test programs
+    add_library( libgtest IMPORTED STATIC GLOBAL )
+    add_dependencies( libgtest gtest )
+
+    # Set libgtest properties
+    set_target_properties( libgtest 
+    					   PROPERTIES
+    					   "IMPORTED_LOCATION" "${binary_dir}/googlemock/gtest/libgtest.a"
+    					   "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+    					 )
+
+    include_directories("${source_dir}/googletest/include"
+    	                "${source_dir}/googlemock/include")
+endif() # DEFINED GTEST_FORCE_EXTERNAL
