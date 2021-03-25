1d419609c2ec70c8ef3318677f0a4f7da6088a52
iirthw <iirthw@gmail.com>
2021-03-25 21:44:50 +0200

    [cmake] A chunk of work on GoogleTest lib download at CMake's configure step

--- cmake/configure_gtest.cmake
+++ cmake/configure_gtest.cmake
@@ -41,31 +41,5 @@ if ( ${FIND_GTEST} )
 endif()
 
 if ( DEFINED GTEST_FORCE_EXTERNAL )
-	# this module is required for ExternalProject_Add
-	include( ExternalProject )
-	# Download GTest source and add it as an external project to the current target
-	ExternalProject_Add(
-    	gtest
-    	URL https://github.com/google/googletest/archive/master.zip
-    	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    	# Disable install step
-    	INSTALL_COMMAND ""
-    )
-
-    # Get GTest source and binary directories from CMake project
-    ExternalProject_Get_Property(gtest source_dir binary_dir)
-
-    # Create a libgtest target to be used as a dependency by test programs
-    add_library( libgtest IMPORTED STATIC GLOBAL )
-    add_dependencies( libgtest gtest )
-
-    # Set libgtest properties
-    set_target_properties( libgtest 
-    					   PROPERTIES
-    					   "IMPORTED_LOCATION" "${binary_dir}/googlemock/gtest/libgtest.a"
-    					   "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    					 )
-
-    include_directories("${source_dir}/googletest/include"
-    	                "${source_dir}/googlemock/include")
+    include( ${PROJECT_SOURCE_DIR}/cmake/download_gtest.cmake )
 endif() # DEFINED GTEST_FORCE_EXTERNAL
