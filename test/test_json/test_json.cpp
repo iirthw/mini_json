e76fed5af3ba5d8a892bf0bf1404294e94b09c3d
iirthw <iirthw@gmail.com>
2021-03-26 15:11:01 +0200

    Test class stub

--- test/test_json/test_json.cpp
+++ test/test_json/test_json.cpp
@@ -1,3 +1,12 @@
 #include "gtest/gtest.h"
 
 #include "json.h"
+
+class QueueTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        
+    }
+
+    // void TearDown() override {}
+};
