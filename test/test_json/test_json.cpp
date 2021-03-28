772cd2e356569adbe55df46c01529d9970aa63ca
iirthw <iirthw@gmail.com>
2021-03-28 21:30:40 +0300

    Example json schema

--- test/test_json/test_json.cpp
+++ test/test_json/test_json.cpp
@@ -2,6 +2,27 @@
 
 #include "json.h"
 
+/*
+Example Json schema:
+{
+  "type": "object",
+  "properties": {
+    "first_name": { "type": "string" },
+    "last_name": { "type": "string" },
+    "birthday": { "type": "string", "format": "date" },
+    "address": {
+      "type": "object",
+      "properties": {
+        "street_address": { "type": "string" },
+        "city": { "type": "string" },
+        "state": { "type": "string" },
+        "country": { "type" : "string" }
+      }
+    }
+  }
+}
+*/
+
 class JsonTest : public ::testing::Test {
 protected:
     void SetUp() override {
