54cc4bb881211232d7aec80bf53ef7b7015d2830
iirthw <iirthw@gmail.com>
2021-03-29 22:44:06 +0300

    [style] Method's comment in the header file

--- lib/impl/string_utils.cpp
+++ lib/impl/string_utils.cpp
@@ -6,16 +6,6 @@ namespace minijson
 {
     namespace utils
     {
-        /*
-            Remove white-space characters.  
-            For the "C" locale, white-space characters are any of:  
-            ' '   (0x20)  space (SPC)
-            '\t'    (0x09)  horizontal tab (TAB)
-            '\n'    (0x0a)  newline (LF)
-            '\v'    (0x0b)  vertical tab (VT)
-            '\f'    (0x0c)  feed (FF)
-            '\r'    (0x0d)  carriage return (CR
-        */
         std::string removeSeparators(const std::string& str)
         {
             return str.erase(std::remove_if(str.begin(), str.end(), std::isspace));
--- lib/public/string_utils.h
+++ lib/public/string_utils.h
@@ -8,6 +8,17 @@ namespace minijson
         {
             public:
 
+                /**
+                 * @brief Remove all white-space characters in the input string.
+                 *
+                 * For the "C" locale, white-space characters are any of:  
+                 * ' '   (0x20)  space (SPC)
+                 * '\t'    (0x09)  horizontal tab (TAB)
+                 * '\n'    (0x0a)  newline (LF)
+                 * '\v'    (0x0b)  vertical tab (VT)
+                 * '\f'    (0x0c)  feed (FF)
+                 * '\r'    (0x0d)  carriage return (CR
+                 */
                 static std::string removeSeparators(const std::string& str);
         }; // class StringUtils
     } // namespace utils
