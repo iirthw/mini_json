15ec10c920f8d98064286082d8b583a8c913719a
iirthw <iirthw@gmail.com>
2021-03-31 18:58:45 +0300

    [string_utils] Get rid of namespace utils, which seems redundant

--- lib/impl/string_utils.cpp
+++ lib/impl/string_utils.cpp
@@ -4,14 +4,11 @@
 
 namespace minijson
 {
-    namespace utils
+    void StringUtils::removeWhitespace(std::string& input)
     {
-        void StringUtils::removeWhitespace(std::string& input)
-        {
-            input.erase(
-                std::remove_if(input.begin(), input.end(), std::isspace),
-                input.end()
-            );
-        }
-    } // namespace utils
+        input.erase(
+            std::remove_if(input.begin(), input.end(), std::isspace),
+            input.end()
+        );
+    }
 } // namespace minijson
--- lib/public/string_utils.h
+++ lib/public/string_utils.h
@@ -4,25 +4,21 @@
 
 namespace minijson
 {
-    namespace utils
+    class StringUtils
     {
-        class StringUtils
-        {
-            public:
-
-                /**
-                 * @brief A helper static member function which remove
-                 * all white-space characters from the input string.
-                 *
-                 * For the "C" locale, white-space characters are any of:  
-                 * ' '   (0x20)  space (SPC)
-                 * '\t'    (0x09)  horizontal tab (TAB)
-                 * '\n'    (0x0a)  newline (LF)
-                 * '\v'    (0x0b)  vertical tab (VT)
-                 * '\f'    (0x0c)  feed (FF)
-                 * '\r'    (0x0d)  carriage return (CR
-                 */
-                static void removeWhitespace(std::string& str);
-        }; // class StringUtils
-    } // namespace utils
+        public:
+            /**
+            * @brief A helper static member function which remove
+            * all white-space characters from the input string.
+            *
+            * For the "C" locale, white-space characters are any of:  
+            * ' '   (0x20)  space (SPC)
+            * '\t'    (0x09)  horizontal tab (TAB)
+            * '\n'    (0x0a)  newline (LF)
+            * '\v'    (0x0b)  vertical tab (VT)
+            * '\f'    (0x0c)  feed (FF)
+            * '\r'    (0x0d)  carriage return (CR
+            */
+            static void removeWhitespace(std::string& str);
+    }; // class StringUtils
 } // namespace minijson
