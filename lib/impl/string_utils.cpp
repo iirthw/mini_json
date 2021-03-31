55e337712adf246ee9b4d820e438b924e74f27d0
iirthw <iirthw@gmail.com>
2021-03-31 19:35:04 +0300

    [string_utils] Copy must be done in any case --> pass by value

--- lib/impl/string_utils.cpp
+++ lib/impl/string_utils.cpp
@@ -4,11 +4,13 @@
 
 namespace minijson
 {
-    void StringUtils::removeWhitespace(std::string& input)
+    // Pass by value is intentional (instead of const &) since the method 
+    // needs a copy of the original input string anyway. Returning by value
+    // will also benefit from NRVO in our particular case.
+    std::string StringUtils::removeWhitespace(std::string str)
     {
-        input.erase(
-            std::remove_if(input.begin(), input.end(), std::isspace),
-            input.end()
-        );
+        str.erase(std::remove_if(str.begin(), str.end(), std::isspace),
+                  str.end());
+        return str;
     }
 } // namespace minijson
--- lib/public/string_utils.h
+++ lib/public/string_utils.h
@@ -9,7 +9,9 @@ namespace minijson
         public:
             /**
             * @brief A helper static member function which remove
-            * all white-space characters from the input string.
+            *        all white-space characters from the input string.
+            * @return the string which is obtained from the input string
+            *         by removing all white space characters.
             *
             * For the "C" locale, white-space characters are any of:  
             * ' '   (0x20)  space (SPC)
@@ -19,6 +21,6 @@ namespace minijson
             * '\f'    (0x0c)  feed (FF)
             * '\r'    (0x0d)  carriage return (CR
             */
-            static void removeWhitespace(std::string& str);
+            static std::string removeWhitespace(std::string str); // Intentionally pass by value as we actually need a copy
     }; // class StringUtils
 } // namespace minijson
