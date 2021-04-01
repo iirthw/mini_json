d1dcb3f0277f3a011dd4ba2b00f09620725a98b3
iirthw <iirthw@gmail.com>
2021-04-01 11:39:45 +0300

    [json] Fix compilation and reiterate on the api

--- lib/impl/json.cpp
+++ lib/impl/json.cpp
@@ -6,15 +6,43 @@
 
 namespace minijson
 {
-    // Class to represent internal json objects hierarchy
+    // Internal json hierarchy representation
+    class JsonObject;
+
+    // PIMPL for the Json class
     struct Json::JsonImpl
     {
-        explicit JsonImpl()
-        : mRawData(utils::removeSeparators(data))
-        , mChildren({}) {}
+        explicit JsonImpl(const std::string& data)
+        : mRawData(StringUtils::removeWhitespace(data))
+        {}
 
-        std::string mRawData;            
-        std::vector<std::unique_ptr<JsonIntern>> mChildren;
+        std::shared_ptr<JsonObject>
+            getObject(std::shared_ptr<JsonObject> obj, const std::string& key) const
+        {
+            return nullptr;
+        }
+
+        bool getBool(std::shared_ptr<JsonObject> obj, const std::string& key) const
+        {
+            return false;
+        }
+
+        int	getInt(std::shared_ptr<JsonObject> obj, const std::string& key) const
+        {
+            return 0;
+        }
+
+        float getFloat(std::shared_ptr<JsonObject> obj, const std::string& key) const
+        {
+            return 0.0f;
+        }
+
+        std::string getString(std::shared_ptr<JsonObject> obj, const std::string& key) const
+        {
+            return "";
+        }
+
+        std::string mRawData;
     };
 
     // =========================================================================
@@ -27,4 +55,35 @@ namespace minijson
     Json::~Json() = default;
     Json::Json(Json&& rhs) noexcept = default;
     Json& Json::operator= (Json&& rhs) noexcept = default;
+
+    std::shared_ptr<JsonObject> 
+        Json::getObject(std::shared_ptr<JsonObject> obj, const std::string& key) const
+    {
+        return mImpl->getObject(obj, key);
+    }
+
+    bool Json::getBool(std::shared_ptr<JsonObject> obj, const std::string& key) const
+    {
+        return mImpl->getBool(obj, key);
+    }
+
+    int	Json::getInt(std::shared_ptr<JsonObject> obj, const std::string& key) const
+    {
+        return mImpl->getInt(obj, key);
+    }
+
+    float Json::getFloat(std::shared_ptr<JsonObject> obj, const std::string& key) const
+    {
+        return mImpl->getFloat(obj, key);
+    }
+
+    std::string Json::getString(std::shared_ptr<JsonObject> obj, const std::string& key) const
+    {
+        return mImpl->getString(obj, key);
+    }
+
+    // =========================================================================
+    // JsonObject
+    // =========================================================================
+
 } // namespace minijson
--- lib/public/json.h
+++ lib/public/json.h
@@ -5,32 +5,31 @@
 
 namespace minijson
 {
+	class JsonObject;
+
 	class Json
 	{
 		public:
 		
 			explicit Json(const std::string& data);
+			// Intentionally declarations only 
+			// for the dtor, move ctor, move assignment operator
 			~Json();
 			Json(Json&& rhs) noexcept;
 			Json& operator= (Json&& rhs) noexcept;
 
-			// FIXME: all of the following have dummy implementation
-
-			explicit Json(const std::string& data);
-
-			Json parse(const std::string& attrib) const { return Json(""); }
-			bool getBool(const std::string& key) const { return false; }
-			int getInt(const std::string& str) const { return 0; }
-			float getFloat(const std::string& str) const { return 0.0f; }
-			std::string getString(const std::string& str) const { return ""; }
-			int getInt(const std::string& key) const;
-			float getFloat(const std::string& key) const { return 0.0f; }
-			std::string getString(const std::string& key) const { return ""; }
+			std::shared_ptr<JsonObject> 
+				getObject(std::shared_ptr<JsonObject> obj,const std::string& key) const;
+			bool getBool(std::shared_ptr<JsonObject> obj, const std::string& key) const;
+			int getInt(std::shared_ptr<JsonObject> obj, const std::string& key) const;
+			float getFloat(std::shared_ptr<JsonObject> obj, const std::string& key) const;
+			std::string getString(std::shared_ptr<JsonObject> obj, const std::string& key) const;
 
 			size_t numObjects() const { return 0u; }
 			Json operator[] (int index) const { return Json(""); }
 
 		private:
+			
 			static std::string readTextFile(const std::string& filePath) { return ""; }
 			static Json fromFile(const std::string& filePath) { return Json(""); }
 
