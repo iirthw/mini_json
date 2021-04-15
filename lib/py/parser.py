d2f20f2d9351894490643624fb4203a5ecf704c4
iirthw <iirthw@gmail.com>
2021-04-15 22:23:54 +0300

    wip on parser (part 1)

--- lib/py/parser.py
+++ lib/py/parser.py
@@ -1,8 +1,13 @@
 s = ['{', 'name', ':', 'GSP', ',', 'age', ':', '39', 'isChamp', ':', 'true', '}']
 
 LEFT_BRACE = '{'
+RIGHT_BRACE = '}'
 LEFT_BRACKET = '['
+RIGHT_BRACKET = ']'
 
+# recursively parse tokens
+# each of the subroutines parse_array and parse_object will recursively call
+# parse (unless simple cases when further recursion might not be required)
 def parse(tokens):
     t = tokens[0]
     if t == LEFT_BRACKET:
@@ -12,12 +17,33 @@ def parse(tokens):
     else:
         return t, tokens[1:]
 
-def parse_object(s):
+# ==============================================================================
+#
+# ==============================================================================
+def parse_object(tokens):
     pass
 
-def parse_array(s):
-    pass
+# ==============================================================================
+#
+# ==============================================================================
+def parse_array(tokens):
+    json_array = []
+    t0 = t[0]
+    if t0 == RIGHT_BRACKET:
+        return json_array, tokens[1:]
 
+    while True:
+        json_array.append(t0)
+        tokens = tokens[1:]
+
+        t = tokens[0]
+        if t == RIGHT_BRACKET:
+            
+
+
+# ==============================================================================
+#
+# ==============================================================================
 def main():
     print("ololol")
 
